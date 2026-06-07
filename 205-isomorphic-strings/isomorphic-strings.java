8c00954049bb5845f66a72863d1b9c25f62d3b90
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-06-07 10:43:57 +0530

    Time: 8 ms (73.67%) | Memory: 44 MB (41.06%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,39 +1,22 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        // Base case: if lengths don't match, they can't be isomorphic
-        if (s.length() != t.length()) {
-            return false;
-        }
-        
-        // Map to track s -> t mappings
-        Map<Character, Character> sToT = new HashMap<>();
-        // Map to track t -> s mappings (ensures 1-to-1 uniqueness)
-        Map<Character, Character> tToS = new HashMap<>();
-        
+        if (s.length() != t.length()) return false;
+
+        // Arrays to store the last seen index + 1 of each character
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
         for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-            
-            // Check if charS already has a mapping
-            if (sToT.containsKey(charS)) {
-                if (sToT.get(charS) != charT) {
-                    return false; // Mismatched mapping found
-                }
-            } else {
-                // If charS is new, make sure charT hasn't been claimed yet
-                if (tToS.containsKey(charT)) {
-                    return false; // Many-to-one mapping attempt
-                }
-                
-                // Establish the bidirectional mapping
-                sToT.put(charS, charT);
-                tToS.put(charT, charS);
+            // If the previous positions don't match, they aren't isomorphic
+            if (mapS[s.charAt(i)] != mapT[t.charAt(i)]) {
+                return false;
             }
+            
+            // Record the current position (i + 1 to avoid default 0 conflicts)
+            mapS[s.charAt(i)] = i + 1;
+            mapT[t.charAt(i)] = i + 1;
         }
-        
+
         return true;
     }
 }
