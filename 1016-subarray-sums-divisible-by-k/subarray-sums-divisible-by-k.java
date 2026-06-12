9f5e295698a7c8c213a921b58b57d493eb4cadcc
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-06-12 11:23:54 +0530

    Time: 26 ms (27.60%) | Memory: 51.6 MB (43.23%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,27 +1,22 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        // Since remainders can only be from 0 to k-1, an array acts as a perfect, ultra-fast map
-        int[] remainderCounts = new int[k];
-        
-        // Base case: remainder 0 has appeared 1 time initially
-        remainderCounts[0] = 1;
-        
-        int runningSum = 0;
-        int totalSubarrays = 0;
-        
-        for (int num : nums) {
-            runningSum += num;
-            
-            // Normalize remainder to be positive
-            int remainder = (runningSum % k + k) % k;
-            
-            // Add the number of times this remainder has been seen before
-            totalSubarrays += remainderCounts[remainder];
-            
-            // Increment the count for the current remainder
-            remainderCounts[remainder]++;
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        hmap.put(0,1);
+        int rem=0;
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+            rem=sum%k;
+            if(rem<0)
+                rem+=k;
+            if(hmap.containsKey(rem))
+            {
+                ans+=hmap.get(rem);
+            }
+            hmap.put(rem,hmap.getOrDefault(rem,0)+1);
         }
-        
-        return totalSubarrays;
+        return ans;
     }
 }
