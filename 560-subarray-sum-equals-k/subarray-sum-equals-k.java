c7e1f7a3688299910b9b7b74d8f56ec877c61120
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-06-12 09:46:51 +0530

    Time: 28 ms (25.63%) | Memory: 49 MB (34.40%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,30 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // Map to store (prefix_sum, frequency_of_occurrence)
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        // Base case: A prefix sum of 0 has occurred 1 time (before any elements)
-        map.put(0, 1);
-        
-        int currentSum = 0;
-        int count = 0;
-        
-        for (int num : nums) {
-            // Update the running prefix sum
-            currentSum += num;
-            
-            // Check if (currentSum - k) exists in our map
-            if (map.containsKey(currentSum - k)) {
-                count += map.get(currentSum - k);
-            }
-            
-            // Add/Update the current prefix sum in the map
-            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
+       Map<Integer,Integer> hmap=new HashMap<Integer,Integer>();
+       int count=0;
+       int sum=0;
+       hmap.put(0,1);
+       for(int i=0;i<nums.length;i++)
+       {
+        sum+=nums[i];
+        int dif=sum-k;
+        if(hmap.containsKey(dif))
+        {
+            count+=hmap.get(dif);
         }
-        
-        return count;
+        hmap.put(sum,hmap.getOrDefault(sum,0)+1);
+       }
+       return count;
     }
 }
