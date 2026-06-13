e3014a0982146bdbc158b76fba64cb2e4e68040d
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-06-13 10:50:52 +0530

    Time: 24 ms (18.84%) | Memory: 52.7 MB (17.48%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,24 +1,17 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
-    }
-
-    public int atMost(int[] nums, int goal) {
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int count = 0;
-        int n = nums.length;
-        if(goal<0)
-            return 0;
-        while (j < n) {
-            sum += nums[j];
-            while (sum > goal) {
-                sum -= nums[i];
-                i++;
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        hmap.put(0,1);
+        int count=0;
+        int sum=0;
+        for(int num:nums)
+        {
+            sum+=num;
+            if(hmap.containsKey(sum-goal))
+            {
+                count+=hmap.get(sum-goal);
             }
-            count += j - i + 1;
-            j++;
+            hmap.put(sum,hmap.getOrDefault(sum,0)+1);
         }
         return count;
     }
