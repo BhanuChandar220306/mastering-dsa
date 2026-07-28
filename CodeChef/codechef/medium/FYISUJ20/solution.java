dfde4de6ddf32454ad5548b56f0f9970a91c8bbe
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-07-28 19:54:59 +0530

    ✅ FYISUJ20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-28

---
+++ CodeChef/codechef/medium/FYISUJ20/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign population values
+        int cityPopulation=5_000_000;
+        int districtPopulation=25_000;
+
+
+        // Print the population values
+        System.out.println("City population: "+cityPopulation);
+        System.out.println("District Population: "+districtPopulation);
+
+    }
+}
