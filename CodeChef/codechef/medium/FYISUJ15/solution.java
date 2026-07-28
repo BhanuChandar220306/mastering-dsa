22b44dea4ec250ff8de47cb44b8f0f86633de692
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-07-28 19:51:58 +0530

    ✅ FYISUJ15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-28

---
+++ CodeChef/codechef/medium/FYISUJ15/solution.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main(String[] args) {
+        //  Declare and assign earthquake intensity readings
+
+        short regionOneIntensity=12000;
+        short regionTwoIntensity=-5000;
+        byte regionThreeIntensity=0;
+
+
+        //  Print the intensity values
+        System.out.println("Region One Intensity: "+regionOneIntensity);
+        System.out.println("Region Two Intensity: "+regionTwoIntensity);
+        System.out.println("Region Three Intensity: "+regionThreeIntensity);
+
+
+    }
+}
