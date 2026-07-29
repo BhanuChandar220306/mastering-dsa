8e6b1962b242ec80c745d5adc7b94e10eb7275a0
Bhanu Chandar <bhanuchandarchagantipati@gmail.com>
2026-07-30 05:12:44 +0530

    Delete CodeChef/codechef directory

--- CodeChef/codechef/medium/FYISUJ14/solution.cpp
+++
@@ -1,9 +0,0 @@
-class Codechef {
-    public static void main(String[] args) {
-        // Declare and assign a short variable
-        short temperature = -150; 
-        
-        // Print the value of the variable
-        System.out.println("Temperature: " + temperature);
-    }
-}
--- CodeChef/codechef/medium/FYISUJ19/solution.cpp
+++
@@ -1,9 +0,0 @@
-class Codechef {
-    public static void main(String[] args) {
-        // Declare and assign an int variable
-        int population = 1_500_000; 
-        
-        // Print the value of the variable
-        System.out.println("City Population: " + population);
-    }
-}
